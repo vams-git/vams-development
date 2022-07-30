5333c716a3276e573e9145098873ced26ebdb2c5
vams-git <109448159+vams-git@users.noreply.github.com>
2022-07-30 16:44:05 +0930

    Create R5MAINTENANCEPATTERNS_5_Post_Insert.sql

---
+++ inactive/R5MAINTENANCEPATTERNS_5_Post_Insert.sql
@@ -0,0 +1,25 @@
+DECLARE
+  mtp R5MAINTENANCEPATTERNS%ROWTYPE;
+  err EXCEPTION;
+  imsg VARCHAR2(400);
+
+BEGIN
+  SELECT * INTO mtp FROM R5MAINTENANCEPATTERNS
+  WHERE ROWID = :rowid;
+  
+  IF nvl(mtp.mtp_udfchkbox04,'-') = '-' THEN
+    UPDATE R5MAINTENANCEPATTERNS
+    SET   mtp_udfchkbox04 = '+'
+    WHERE mtp_org = mtp.mtp_org
+      AND mtp_code = mtp.mtp_code
+      AND mtp_revision = mtp.mtp_revision;
+  END IF;
+
+EXCEPTION
+  WHEN err THEN
+    RAISE_APPLICATION_ERROR (-20003, imsg);
+  WHEN OTHERS THEN
+    Raise_application_error (-20003,
+      'Error in Flex R5MAINTENANCEPATTERNS/Post Insert/5/'
+      ||Substr(SQLERRM, 1, 500));
+END;
