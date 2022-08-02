a4afa248511244d4aba2424dd0004958501c30ab
vams-git <109448159+vams-git@users.noreply.github.com>
2022-08-02 11:10:09 +0930

    db mutating fix
    - nullified restart mp fields (udfchar02,04,05)
    - update error prefix

--- inactive-field/flex/R5PATTERNEQUIPMENT_20_Post_Insert.sql
+++ inactive-field/flex/R5PATTERNEQUIPMENT_20_Post_Insert.sql
@@ -11,7 +11,9 @@ BEGIN
   IF peq.peq_status != 'I' THEN
     UPDATE R5MAINTENANCEPATTERNS
     SET   mtp_udfchkbox04 = NULL,
-          mtp_udfchkbox05 = NULL
+          mtp_udfchar02 = NULL,
+          mtp_udfchar04 = NULL,
+          mtp_udfchar05 = NULL
     WHERE mtp_org = peq.peq_mp_org
       AND mtp_code = peq.peq_mp
       AND mtp_revision = peq.peq_revision;
@@ -22,6 +24,6 @@ EXCEPTION
   RAISE_APPLICATION_ERROR (-20003, imsg);
   WHEN OTHERS THEN
   RAISE_APPLICATION_ERROR (-20003,
-    'ERR R5PATTERNEQUIPMENT/20/Post Insert/'
+    'ERR/R5PATTERNEQUIPMENT/20/Insert/'
     ||Substr(SQLERRM, 1, 500));
 END;
