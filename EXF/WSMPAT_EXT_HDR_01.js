eae8569a7a4f0c9a447e3c0ac7a62f6d1d298d06
vams-git <109448159+vams-git@users.noreply.github.com>
2022-08-03 21:40:25 +0930

    fix popup error when browsing tabs vs list view

--- EXF/WSMPAT_EXT_HDR_01.js
+++ EXF/WSMPAT_EXT_HDR_01.js
@@ -1,14 +1,26 @@
 '[extensibleFramework] [tabName=HDR][isTabView=true]': {
-	afterlayout: function () {
-		if (!EAM.Utils.getScreen().isScreenDesigner) {
-			var vFormPanel = this.getFormPanel();
-			vFormPanel.getFld('udfchkbox04').setReadOnly(true);
-		}
-	},
-	afterrecordchange: function () {
-		if (!EAM.Utils.getScreen().isScreenDesigner) {
-			var vFormPanel = this.getFormPanel();
-			vFormPanel.getFld('udfchkbox04').setReadOnly(true);
-		}
-	},
+  afterlayout: function () {
+    if (!EAM.Utils.getScreen().isScreenDesigner) {
+      var vFormPanel = this.getFormPanel();
+      if (EAM.Utils.getScreen().getCurrentTab().tabName == 'HDR') {
+        // if only on HDR
+        FormPanel.getFld('udfchkbox04').setReadOnly(true)
+      }
+      /*
+      SOME OTHER CODES
+      */
+    }
+  },
+  afterrecordchange: function () {
+    if (!EAM.Utils.getScreen().isScreenDesigner) {
+      var vFormPanel = this.getFormPanel();
+      if (EAM.Utils.getScreen().getCurrentTab().tabName == 'HDR') {
+        // if only on HDR
+        FormPanel.getFld('udfchkbox04').setReadOnly(true)
+      }
+      /*
+      SOME OTHER CODES
+      */
+    }
+  },
 },
