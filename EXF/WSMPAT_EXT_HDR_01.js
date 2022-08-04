018e47da5537babb859b414e5cf347db42d10705
vams-git <109448159+vams-git@users.noreply.github.com>
2022-08-04 16:23:42 +0930

    Update WSMPAT_EXT_HDR_01.js

--- EXF/WSMPAT_EXT_HDR_01.js
+++ EXF/WSMPAT_EXT_HDR_01.js
@@ -1,26 +1,28 @@
+/* some other codes */
 '[extensibleFramework] [tabName=HDR][isTabView=true]': {
   afterlayout: function () {
     if (!EAM.Utils.getScreen().isScreenDesigner) {
       var vFormPanel = this.getFormPanel();
       if (EAM.Utils.getScreen().getCurrentTab().tabName == 'HDR') {
-        // if only on HDR
-        FormPanel.getFld('udfchkbox04').setReadOnly(true)
+        /* some other codes */
+        inactive_init(vFormPanel);
+        /* some other codes */
       }
-      /*
-      SOME OTHER CODES
-      */
     }
   },
   afterrecordchange: function () {
     if (!EAM.Utils.getScreen().isScreenDesigner) {
       var vFormPanel = this.getFormPanel();
       if (EAM.Utils.getScreen().getCurrentTab().tabName == 'HDR') {
-        // if only on HDR
-        FormPanel.getFld('udfchkbox04').setReadOnly(true)
+        /* some other codes */
+        inactive_init(vFormPanel);
+        /* some other codes */
       }
-      /*
-      SOME OTHER CODES
-      */
     }
   },
 },
+/* some other codes */
+function inactive_init(vFormPanel) {
+  try { vFormPanel.getFld('udfchkbox04').setReadOnly(true) }
+  catch (err) { console.log('inactive_init'); console.log(err.toString()) }
+}
